b50c747756ff7d67a3d6b07edb366e5ef2b9dad0
Parla Mehdivand <parlamahdivand88@gmail.com>
2025-05-08 18:44:08 +0430

    Update Matrix_Operations.py

--- Matrix_Operations.py
+++ Matrix_Operations.py
@@ -1,9 +1,31 @@
 import numpy as np
 
-matrix = np.array([
-    [1, 2, 3, 4],
-    [5, 6, 7, 8],
-    [9, 10, 11, 12]
-])
+# Define two 3x3 matrices with mixed positive and negative numbers
+A = np.array([[ 4, -7,  2],
+              [-3,  5,  9],
+              [ 8, -6, -1]])
 
-print(matrix[1, 2])
+B = np.array([[-2,  3, -5],
+              [ 7, -8,  4],
+              [-9,  1,  6]])
+
+# Matrix addition
+matrix_add = A + B
+
+# Matrix subtraction
+matrix_sub = A - B
+
+# Matrix multiplication (dot product)
+matrix_mult = A @ B # the same as "np.dot(A, B)"
+
+# Print results
+print("Matrix A:")
+print(A)
+print("\nMatrix B:")
+print(B)
+print("\nA + B:")
+print(matrix_add)
+print("\nA - B:")
+print(matrix_sub)
+print("\nA * B:")
+print(matrix_mult)
